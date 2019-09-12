f64a11f3727859b6689f8cf326f7796c6f08cc9f
Nicksechko <sechko2000@mail.ru>
2019-09-12 20:39:51 +0300

    Add List Tests

---
+++ test.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "list.h"
+#include <vector>
+#include <map>
+#include <string>
+#include <exception>
+
+void TestEmptyList();
+
+void TestIterator();
+
+void TestFindBack();
+
+void TestFindFront();
+
+void TestFindBackAndFront();
+
+void TestAsArrray();
+
+void TestFindWithPredicate();
+
+void TestInsertAfterAndErase();
+
+void TestInsertBeforeAndErase();
+
+void TestAll();
